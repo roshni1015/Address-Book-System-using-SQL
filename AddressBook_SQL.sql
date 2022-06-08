248681b3bf5979c19ee860a9cfef340b747ebccc
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-08 16:16:15 +0530

    [Roshni]-Instering New Contacts

--- AddressBook_SQL.sql
+++ AddressBook_SQL.sql
@@ -14,3 +14,13 @@ ZipCode varchar(10),
 PhoneNumber varchar(20),
 EmailId varchar(100)
 );
+
+/* UC3 Insert New Contacts to Adress Book */
+Insert into PersonContact(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId) values 
+('Roshni','Adatrao','S.T.Colony','Pune','Maharastra','123456','9999786747','roshni@mail.com'),
+('Lavanya','Chaudhary','B.T.Colony','banglore','Karnataka','789123','9909258899','lavanya@mail.com'),
+('Mahesh','Deshmukh','R.T.Colony','Mumbai','Maharastra','345678','8877927846','mahesh@mail.com'),
+('Vivek','Patil','A.T.Colony','Chennai','Tamil Nadu','234517','9856112233','vivek@mail.com'),
+('Priya','Kulkarni','H.T.Colony','Kolkata','West Bangal','908745','7775137805','priya@mail.com');
+
+select * from PersonContact;
