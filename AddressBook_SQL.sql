1af281f9f6bbd74eaeceed6414b154a8aaa05c69
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-08 16:14:20 +0530

    [Roshni]-Create a Table For First And Last name, Address, City, State, Zip, PhoneNumber, Email

--- AddressBook_SQL.sql
+++ AddressBook_SQL.sql
@@ -1,2 +1,16 @@
+/*UC1 to create a Address Book service database*/
 create database AddressBookDB
 use AddressBookDB
+
+/* UC2 create a PersonContacts table in theAddress Book service database*/
+create table PersonContact(
+Id int identity(1,1) NOT NULL PRIMARY KEY,
+FirstName varchar(20) ,
+LastName varchar(20),
+Address varchar(200),
+City varchar(20),
+State varchar(20),
+ZipCode varchar(10),
+PhoneNumber varchar(20),
+EmailId varchar(100)
+);
