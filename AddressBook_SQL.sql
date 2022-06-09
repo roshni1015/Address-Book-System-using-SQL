7a2f2961a35893469c8a433275424155eecb4187
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-09 11:49:56 +0530

    [Roshni]-Retrieve Entries Sorted Alphabetically by Person's Name for a given City

--- AddressBook_SQL.sql
+++ AddressBook_SQL.sql
@@ -18,7 +18,7 @@ EmailId varchar(100)
 /* UC3 Insert New Contacts to Adress Book */
 Insert into PersonContact(FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId) values 
 ('Roshni','Adatrao','S.T.Colony','Pune','Maharastra','123456','9999786747','roshni@mail.com'),
-('Lavanya','Chaudhary','B.T.Colony','banglore','Karnataka','789123','9909258899','lavanya@mail.com'),
+('Lavanya','Chaudhary','B.T.Colony','Pune','Karnataka','789123','9909258899','lavanya@mail.com'),
 ('Mahesh','Deshmukh','R.T.Colony','Mumbai','Maharastra','345678','8877927846','mahesh@mail.com'),
 ('Vivek','Patil','A.T.Colony','Chennai','Tamil Nadu','234517','9856112233','vivek@mail.com'),
 ('Priya','Kulkarni','H.T.Colony','Kolkata','West Bangal','908745','7775137805','priya@mail.com');
@@ -40,8 +40,11 @@ select * from PersonContact where City = 'Mumbai'
 select * from PersonContact where State = 'Tamil Nadu' 
 
 
-/* Size of Address Book by City and State*/ 
+/* UC7 Size of Address Book by City and State*/ 
 Select count(City) from PersonContact;
 Select COUNT(Address) From PersonContact Where City='Pune' 
 Select count(State) from PersonContact;
 Select COUNT(Address) From PersonContact Where State='Maharastra' 
+
+/* UC8 Retrieve Entries Sorted Alphabetically by Person’s name for a given city */
+Select* from PersonContact where city='Pune'order by FirstName Asc;
